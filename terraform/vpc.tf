f4b4cf46f885ad19551c344a089e28c2739e6b84
Manojkumar080197 <manojkumar.official9894@gmail.com>
2026-03-06 18:46:38 +0530

    Update vpc.tf

---
+++ terraform/vpc.tf
@@ -0,0 +1,29 @@
+resource "aws_vpc" "devops_vpc" {
+  cidr_block = var.vpc_cidr
+
+  tags = {
+    Name = "devops-vpc"
+  }
+}
+
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id                  = aws_vpc.devops_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-south-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet-1"
+  }
+}
+
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id                  = aws_vpc.devops_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "ap-south-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet-2"
+  }
+}
